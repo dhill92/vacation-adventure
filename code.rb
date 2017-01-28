caaac380ca2ad42a6f12a2868154160202e3ee99
dhill92 <dhill92@gmail.com>
2017-01-27 21:45:48 -0500

    first commit

---
+++ code.rb
@@ -0,0 +1,18 @@
+puts "Today we are going to go:"
+
+number = rand(3) + 1
+
+ if number == 1 then
+   puts "jet skiing"
+    else
+
+      if number == 2 then
+        puts "sailing"
+      else
+
+        if number == 3 then
+          puts "to an animal reservation"
+
+        end
+      end
+    end
